c5618ecabeac3a626eacaa8ae025f47e3a3f1427
cgrassel <cgras8@gmail.com>
2018-02-13 17:53:00 -0600

    output the file to console

--- project 1.py
+++ project 1.py
@@ -1,6 +1,9 @@
 print("Testing the github update process")
 inputFile = open("test1_bin.txt", "r")
 print("Hello")
-dataList = [inputFile.read().splitlines()]
-print(dataList)
+dataList = inputFile.read().splitlines()
+for i in range (0 , len(dataList)):
+    print(dataList[i])
+
+
 inputFile.close()
